84490b4c4f7a7a143989418ab480c55bb71824cc
Cheef44 <bananchik2211@mail.ru>
2024-04-26 22:04:29 +0300

    feature: сконвертированный факл интерфейса окна для поиска чатов

---
+++ src/registration.py
@@ -0,0 +1,30 @@
+import sys
+import os
+sys.path.append(os.path.abspath('../src'))
+
+from PyQt6.QtWidgets import QWidget, QApplication
+import registration_programm_interface
+import db
+import search_chtas
+
+class Registration_windows(QWidget, registration_programm_interface.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setFixedSize(500, 400)
+        self.Registration.clicked.connect(
+            lambda: self.registration(name=self.nickname.text(), email=self.email.text(), password=self.password.text(), password_2=self.password_2.text())
+            )
+        
+    def registration(self, name, email, password, password_2):
+        if password == password_2:
+            user = db.DataBase(password, name, email)
+            user.create_db()
+            self.open_search_chats()
+        else:
+            self.password.setText('Пароли различаются')
+    
+    def open_search_chats(self):
+        self.close()
+        self.window = search_chtas.Search_window()
+        self.window.show()
