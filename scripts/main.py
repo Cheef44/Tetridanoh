fc4c00fe23777190899c9392ee4ef97ff6551377
Cheef44 <bananchik2211@mail.ru>
2024-04-26 23:18:13 +0300

    feature: подключение к серверу

--- scripts/main.py
+++ scripts/main.py
@@ -5,12 +5,17 @@ sys.path.append(os.path.abspath('src'))
 from src import registration, search_chtas
 from src import db
 from PyQt6.QtWidgets import QApplication
+import socket
+import json
+from threading import Thread
 
 def start_app():
+    client  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('DESKTOP-96M1JI1', 1488))
     app = QApplication([])
     database = db.DataBase()
     if database.check_db() == False:
-        windows = registration.Registration_windows()
+        windows = registration.Registration_windows(client)
         windows.show()
     else:
         window = search_chtas.Search_window()
