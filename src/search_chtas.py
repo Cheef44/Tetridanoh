88c2b37816258d3f42d82a58bc82fd24e049cd9f
Cheef44 <bananchik2211@mail.ru>
2024-04-29 18:15:30 +0300

    feature: реализа функции вывода всех дружеских чатов

--- src/search_chtas.py
+++ src/search_chtas.py
@@ -12,11 +12,13 @@ class Search_window(QWidget, search_programm_interface.Ui_MainWindow):
         super().__init__()
         self.server = server
         self.puk = False
-        
         self.setFixedSize(538, 463)
         self.setupUi(self)
         name = db.DataBase().retrieve_data()[0][0][0]
         self.nick_name.setText(name)
+        if self.tabWidget.currentIndex() == 0:
+            self.freands_chats(0)
+        self.tabWidget.currentChanged.connect(self.freands_chats)
         self.tabWidget.currentChanged.connect(self.All_chats)
         self.all_list.itemClicked.connect(self.add_chat)
     
@@ -30,9 +32,12 @@ class Search_window(QWidget, search_programm_interface.Ui_MainWindow):
             self.server.send((json.dumps(["(ALLCHATS)"]).encode('utf-8')))
             server_data = self.server.recv(1024)
             server_data = json.loads(server_data)
+            self.server.send((json.dumps(["(RETRIVEDATA)", db.DataBase().retrieve_data()[-1][0][0]]).encode('utf-8')))
+            id = self.server.recv(1024)
+            id = json.loads(id)
             if server_data:
                 for i in server_data:
-                    if i[1] != self.nick_name.text():
+                    if i[0] != id[0][0]:
                         self.keys.append(i[0])
                         self.items = {k:v for k,v in zip(self.keys,values)}
                         if not i[0] in self.items.keys():
@@ -42,7 +47,7 @@ class Search_window(QWidget, search_programm_interface.Ui_MainWindow):
     def add_chat(self, item):
         index = self.all_list.row(item)
         if self.items:
-            add_chat = QMessageBox.question(None, "Добавление пользователя", f"Хотите ли вы добавить пользователя: {self.items[self.keys[index]]} в дружеские чаты?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+            add_chat = QMessageBox.question(None, "Добавление пользователя", f"Хотите ли вы добавить пользователя {self.items[self.keys[index]]} с id: {self.keys[index]} в дружеские чаты?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
             if add_chat == QMessageBox.StandardButton.Yes:
                 chat_id = self.keys[index]
                 email = ''.join(db.DataBase.retrieve_data(False)[-1][0])
@@ -54,3 +59,20 @@ class Search_window(QWidget, search_programm_interface.Ui_MainWindow):
                     QMessageBox.information(None, "Добавление пользователя", f"Пользователь {self.items[self.keys[index]]} был добавлен в дружеские чаты")
                 else:
                     QMessageBox.information(None, "Добавление пользователя", f"Пользователь {self.items[self.keys[index]]} уже есть у вас в друзьях")
+    
+    def freands_chats(self, index):
+        if index == 0:
+            email = ''.join(db.DataBase.retrieve_data(False)[-1][0])
+            user_data = ['(ALLFREANDS)', email]
+            self.server.sendall(json.dumps(user_data).encode('utf-8'))
+            values = [self.freand_list.item(item_index).text() for item_index in range(self.freand_list.count())]
+            keys = []
+            if user_data:
+                server_data = self.server.recv(1024)
+                server_data = json.loads(server_data)
+                if server_data:
+                    for i in server_data:
+                        keys.append(i[1])
+                        items = {k:v for k,v in zip(keys,values)}
+                        if not i[1] in items.keys():
+                            self.freand_list.addItem(i[0])          
